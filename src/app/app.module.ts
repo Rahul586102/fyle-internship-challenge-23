c70ed751df25c1801361422b095efbbe9fb652ab
Rahul586102 <rahulbiradar914@gmail.com>
2023-11-04 16:16:26 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { ProfileComponent } from './profile/profile.component';
+import {ApiService} from './services/api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
+
+interface GithubRepository {
+  id: number;
+  name: string;
+  // Add other properties you want to access from the GitHub API response
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private username: any;
+  private clientId: any;
+  private clientSecret: any;
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
-
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  constructor(private httpClient: HttpClient) {
+    this.username = 'Rahul586102';
+    this.clientId = '418192';
+    this.clientSecret = 'Iv1.4e2735adc8f855ba';
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getUser(githubUsername: string): Observable<any> {
+    const url = `https://api.github.com/users/${this.username}?client_id=${this.clientId}&client_secret=${this.clientSecret}`;
+    return this.httpClient.get(url);
+  }
+
+  getRepos(githubUsername: string): Observable<GithubRepository[]> {
+    const url = `https://api.github.com/users/${this.username}/repos?client_id=${this.clientId}&client_secret=${this.clientSecret}`;
+    return this.httpClient.get<GithubRepository[]>(url);
+  }
+  updateProfile(username:string){
+  	this.username = username;
+  }
 }
